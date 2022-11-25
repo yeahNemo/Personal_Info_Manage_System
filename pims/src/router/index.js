e3c8a43665f8575f531d24a97daa409d6180d1bc
NEMO <974025642@qq.com>
2022-11-25 10:32:55 +0800

    新增移动端文件夹

--- pims/src/router/index.js
+++ pims/src/router/index.js
@@ -14,6 +14,10 @@ const routes = [
     component: () => import('@/views/Main'),
     children: [
       {
+        path: '/',
+        redirect: 'me'
+      },
+      {
         path: 'friend',
         name: 'friend',
         component: () => import('@/views/Friends')
